112d7f5c788541d8494b54bb8ee8c1a101d21828
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-11-10 12:14:44 +0100

    refactor: npm api queries

--- src/libs/npm.ts
+++ src/libs/npm.ts
@@ -1,16 +1,42 @@
+import fs from 'node:fs/promises'
+import { setTimeout } from 'node:timers/promises'
+
 import { endOfMonth, format, startOfMonth, subMonths } from 'date-fns'
 import fetch from 'node-fetch'
 
+const historyFilePath = './history.json'
+const minRequestInterval = 2000 // In milliseconds.
 const periodFormat = 'yyyy-MM-dd'
 
-export async function fetchNpmDownloads(): Promise<NpmDownloads> {
+export async function fetchRecentNpmDownloads(): Promise<NpmDownloads> {
   const pkgs = await fetchPackages(process.env.NPM_LOGIN)
-  const downloads = await fetchPackagesDownloads(pkgs)
+  const downloads = await fetchRecentNpmPackagesDownloads(pkgs)
 
-  return { all: downloads, total: downloads.reduce((total, { count }) => total + count, 0) }
+  return downloads
 }
 
-async function fetchPackages(author: string): Promise<string[]> {
+export async function persistRecentNpmDownloads(recent: NpmDownloads): Promise<NpmYearDownloads> {
+  const data = await fs.readFile(historyFilePath, 'utf8')
+  const history = JSON.parse(data) as NpmDownloads
+
+  const updatedHistory: NpmDownloads = { ...history, ...recent }
+
+  await fs.writeFile(historyFilePath, JSON.stringify(updatedHistory, null, 2), 'utf8')
+
+  const yearDownloads: NpmYearDownloads = { all: [], total: 0 }
+  const periods = getLastYearPeriods().reverse()
+
+  for (const { date } of periods) {
+    const monthlyDownloads = updatedHistory[date] ?? 0
+
+    yearDownloads.all.push({ count: monthlyDownloads, date: new Date(date) })
+    yearDownloads.total += monthlyDownloads
+  }
+
+  return yearDownloads
+}
+
+async function fetchPackages(author: string): Promise<NpmPackages> {
   const url = new URL('https://registry.npmjs.org/-/v1/search')
   url.search = new URLSearchParams({ size: '250', text: `maintainer:${author}` }).toString()
 
@@ -22,47 +48,104 @@ async function fetchPackages(author: string): Promise<string[]> {
 
   const json = (await response.json()) as { objects: NpmSearchResult[] }
 
-  return json.objects.map((searchResult) => {
-    return searchResult.package.name
-  })
+  const pkgs: string[] = []
+  const scopedPkgs: string[] = []
+
+  for (const searchResult of json.objects) {
+    const name = searchResult.package.name
+    if (searchResult.package.name.startsWith('@')) scopedPkgs.push(name)
+    else pkgs.push(name)
+  }
+
+  return { pkgs, scopedPkgs }
 }
 
-async function fetchPackagesDownloads(pkgs: string[]): Promise<NpmDownloads['all']> {
-  const downloads: Record<string, number> = {}
+async function fetchRecentNpmPackagesDownloads({ pkgs, scopedPkgs }: NpmPackages): Promise<NpmDownloads> {
+  const downloads: NpmDownloads = {}
 
-  const periods = getLastYearPeriods()
+  const { start, end, date } = getCurrentMonthPeriod()
+  const range = `${start}:${end}`
 
-  for (const { start, end, date } of periods) {
-    for (const pkg of pkgs) {
-      const range = `${start}:${end}`
+  const json = await fetchPackagesDownloads(pkgs, range)
 
-      console.info(`Fetching npm downloads for '${pkg}' (range: ${range})`)
+  for (const pkg of pkgs) {
+    const dls = json[pkg]?.downloads || 0
 
-      const response = await fetch(`https://api.npmjs.org/downloads/point/${range}/${pkg}`)
-
-      if (!response.ok) {
-        throw new Error(
-          `${response.status}: ${response.statusText} while fetching npm downloads for '${pkg}' (range: ${range}).`
-        )
-      }
-
-      const json = (await response.json()) as { downloads: number }
-
-      if (downloads[date]) {
-        downloads[date] += json.downloads
-      } else {
-        downloads[date] = json.downloads
-      }
+    if (downloads[date]) {
+      downloads[date] += dls
+    } else {
+      downloads[date] = dls
     }
   }
 
-  return Object.entries(downloads)
-    .map(([date, count]) => ({ count, date: new Date(date) }))
-    .reverse()
+  for (const pkg of scopedPkgs) {
+    const json = await fetchPackagesDownloads([pkg], range)
+    const dls = json[pkg]?.downloads || 0
+
+    if (downloads[date]) {
+      downloads[date] += dls
+    } else {
+      downloads[date] = dls
+    }
+  }
+
+  return downloads
+}
+
+async function fetchPackagesDownloads(pkgs: string[], range: string): Promise<Record<string, { downloads: number }>> {
+  const pkgsStr = pkgs.join(',')
+
+  console.info(`Fetching npm downloads for package(s) [${pkgsStr}] (range: ${range})`)
+
+  const response = await fetch(`https://api.npmjs.org/downloads/point/${range}/${pkgsStr}`)
+
+  if (response.status === 429) {
+    const retryAfter = response.headers.get('Retry-After')
+    const retryAfterMs = retryAfter ? Number.parseInt(retryAfter, 10) * 1000 : undefined
+
+    if (!retryAfterMs || Number.isNaN(retryAfterMs)) {
+      throw new Error(
+        `Rate limited while fetching npm downloads for ['${pkgsStr}] (range: ${range}) but no valid 'Retry-After' header found.`
+      )
+    }
+
+    console.info(`Waiting for ${retryAfter}s before retrying to fetch npm downloads for [${pkgsStr}] (range: ${range})`)
+
+    await setTimeout(retryAfterMs + minRequestInterval)
+
+    return fetchPackagesDownloads(pkgs, range)
+  }
+
+  if (!response.ok) {
+    throw new Error(
+      `${response.status}: ${response.statusText} while fetching npm downloads for ['${pkgsStr}] (range: ${range}).`
+    )
+  }
+
+  await setTimeout(minRequestInterval)
+
+  const json = await response.json()
+
+  return pkgs.length === 1
+    ? { [pkgs[0] as string]: json as { downloads: number } }
+    : (json as Record<string, { downloads: number }>)
+}
+
+function getCurrentMonthPeriod(): Period {
+  const now = new Date()
+
+  const end = now
+  const start = startOfMonth(now)
+
+  return {
+    date: format(start, 'yyyy-MM'),
+    start: format(start, periodFormat),
+    end: format(end, periodFormat),
+  }
 }
 
 function getLastYearPeriods() {
-  const periods: Period[] = []
+  const periods: Period[] = [getCurrentMonthPeriod()]
 
   function addPeriod(startDate: Date, endDate: Date) {
     periods.push({
@@ -77,8 +160,6 @@ function getLastYearPeriods() {
   let end = now
   let start = startOfMonth(now)
 
-  addPeriod(start, end)
-
   for (let i = 1; i <= 12; i++) {
     start = subMonths(start, 1)
     end = endOfMonth(start)
@@ -89,7 +170,17 @@ function getLastYearPeriods() {
   return periods
 }
 
+interface NpmPackages {
+  pkgs: string[]
+  scopedPkgs: string[]
+}
+
 export interface NpmDownloads {
+  // yyyy-MM: count
+  [range: string]: number
+}
+
+export interface NpmYearDownloads {
   all: {
     count: number
     date: Date // yyyy-MM
